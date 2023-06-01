03bc5bcf06e92a934a1fdb1bbc0c8510e8fbb53c
RafalSzurmak <116839542+RafalSzurmak@users.noreply.github.com>
2023-06-01 21:28:30 +0200

    Add files via upload

---
+++ ConsoleApp1/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Monster
+{
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+
+    public Monster(int health, int attack, int defense)
+    {
+        Health = health;
+        Attack = attack;
+        Defense = defense;
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine("Zdrowie potwora: " + Health);
+        Console.WriteLine("Atak potwora: " + Attack);
+        Console.WriteLine("Obrona potwora: " + Defense);
+    }
+}
