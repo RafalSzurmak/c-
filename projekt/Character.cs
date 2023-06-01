2a91d7c8408020fc91fe5f4be3cd5dc009cc821b
RafalSzurmak <116839542+RafalSzurmak@users.noreply.github.com>
2023-06-01 21:33:10 +0200

    Add files via upload

---
+++ projekt/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Character
+{
+    public string Name { get; private set; }
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Mana { get; set; }
+
+    public Character(string name, int health, int attack, int defense, int mana)
+    {
+        Name = name;
+        Health = health;
+        Attack = attack;
+        Defense = defense;
+        Mana = mana;
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine("Nazwa postaci: " + Name);
+        Console.WriteLine("Zdrowie: " + Health);
+        Console.WriteLine("Atak: " + Attack);
+        Console.WriteLine("Obrona: " + Defense);
+        Console.WriteLine("Mana: " + Mana);
+    }
+}
